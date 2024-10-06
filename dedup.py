c0e0c1dac06175d3b35a021db2e7c16090555c90
Pavel Kruchinin <kruchinin.1977@gmail.com>
2024-10-06 20:08:50 +0000

    unified exiftool for pics and videos

--- dedup.py
+++ dedup.py
@@ -9,15 +9,17 @@ def dedup(dup_log: str):
     line_count = 0
     with open(dup_log, "r", encoding='utf-8') as f:
         lines = sorted(f.readlines())
-        last_line, last_hash = '', ''
-        for line in lines:
+        last_line, last_hash, last_name = '', '', ''
+        for idx, line in enumerate(lines):
             line_count = line_count + 1
             hash = regex.split('\\s+', line)[0]
             name = line[len(hash):].strip()
             if last_hash == hash:
+                print(f'#dupes {name} == {last_name}')
                 print(f'mv "{dir}/{name}" dedup/')
             last_hash = hash
             last_line = line
+            last_name = name
     print(f'Done {line_count} lines')
 
 
